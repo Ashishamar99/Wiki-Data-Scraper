94dca879303658ea5e87d01001cc179957c11b6d
Ashishamar99 <ashishamar1999@gmail.com>
2021-01-10 08:05:26 +0530

    Added database code.

--- querydb.py
+++ querydb.py
@@ -1,7 +1,22 @@
 import os
+import sqlite3
 
-if 'queries.txt' in os.listdir():
+if 'resultset.txt' in os.listdir():
+    os.remove('resultset.txt')
+
+# Creating db file if it doesn't exist.
+if 'queries.db' in os.listdir():
     pass
-    
 else:
-    os.system('"echo new db records >> queries.txt"')
+    db_connection = sqlite3.connect('queries.db')
+    my_cursor = db_connection.cursor()
+    my_cursor.execute("CREATE TABLE info (url text, status text)")
+    db_connection.commit()
+    db_connection.close()
+
+db_connection = sqlite3.connect('queries.db')
+my_cursor = db_connection.cursor()
+# Since our database exists, let's fetch the records and dump them into a file called resultset.txt.
+for row in my_cursor.execute("SELECT * FROM info"):
+    print(row)
+#os.system('"echo newer db records >> resultset.txt"')
