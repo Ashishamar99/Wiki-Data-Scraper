20ceff87475b94086f4bc5b7619a73455ea971f3
Ashishamar99 <ashishamar1999@gmail.com>
2021-01-16 18:28:02 +0530

    Seperated logic for database record fetching.

--- JS/main.js
+++ JS/main.js
@@ -1,21 +1,3 @@
-function fetch_db_records()
-{
-    let xhr=new XMLHttpRequest();
-    xhr.open('GET','resultset.json',true);
-    xhr.send();
-    xhr.onload=function(){
-
-        if(xhr.status===200){
-        let data=xhr.responseText;
-        console.log(data);
-            };
-};
-}
-
-$(document).ready(function(){
-        setTimeout(fetch_db_records, 3000);
-});
-
 function sendurltoscrape()
 {
     data = document.getElementById("urlinput").value;
